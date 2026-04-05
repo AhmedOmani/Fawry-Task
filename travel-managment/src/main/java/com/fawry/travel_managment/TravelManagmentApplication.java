b5575366f1667081af638f2b3103df0eed19cded
AhmedSaber <ahmedomani2003@gmail.com>
2026-04-05 21:56:12 +0400

    chore: initialize Spring Boot project

---
+++ travel-managment/src/main/java/com/fawry/travel_managment/TravelManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.fawry.travel_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelManagmentApplication.class, args);
+	}
+
+}
---
+++ travel-managment/src/test/java/com/fawry/travel_managment/TravelManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fawry.travel_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
